f0f68896719827dbba80172dccc4bf516da62f0e
romdotdog <romdotdog@users.noreply.github.com>
2021-04-10 10:00:01 -0500

    Basic file checking

---
+++ src/fileNameIdentifier.ts
@@ -0,0 +1,41 @@
+export enum FileType {
+	Server,
+	Client,
+	Shared
+}
+
+const fileEndingToType: Record<string, FileType | undefined> = {
+	server: FileType.Server,
+	client: FileType.Client,
+	shared: FileType.Shared
+}
+
+export function getFileEndingFromType(fileType: FileType): string {
+	const result = Object.keys(fileEndingToType).find(
+		(fileEnding) => fileEndingToType[fileEnding] === fileType
+	)
+
+	if (!result)
+		throw new Error("Invalid fileType provided for getFileEndingFromType")
+	return result
+}
+
+export default function identifyFromFileName(
+	fileName: string
+): FileType | never {
+	const splitByDot = fileName.split(".")
+	let result
+
+	if (
+		splitByDot.length >= 2 &&
+		(result = fileEndingToType[splitByDot[splitByDot.length - 2]])
+	) {
+		return result
+	}
+
+	throw new Error(
+		`Expected one of ${Object.keys(fileEndingToType).map(
+			(f) => `fileName.${f}.lua`
+		)} while parsing filename ${fileName}.`
+	)
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,42 @@
-console.log("Hello, world!")
+import fs from "fs"
+import { cac } from "cac"
+import identifyFromFileName, {
+	FileType,
+	getFileEndingFromType
+} from "./fileNameIdentifier"
+
+const cli = cac()
+
+async function checkFileExists(file: string) {
+	return fs.promises
+		.access(file, fs.constants.F_OK)
+		.then(() => true)
+		.catch(() => false)
+}
+
+cli
+	.command("[input] <output>", "Bundle this file and all dependencies")
+	.action(async (input: string, output: string | undefined) => {
+		if (!checkFileExists(input)) {
+			throw new Error("Input file not found.")
+		}
+
+		const mainFileType = identifyFromFileName(input)
+
+		if (mainFileType == FileType.Shared) {
+			throw new Error("Main file type cannot be shared.")
+		}
+
+		// Check if output filename is correct
+		if (!output) {
+			output ??= `main.${getFileEndingFromType(mainFileType)}.lua`
+		} else {
+			try {
+				identifyFromFileName(output)
+			} catch {
+				output = `${output.replace(".lua", "")}.${getFileEndingFromType(
+					mainFileType
+				)}.lua`
+			}
+		}
+	})
